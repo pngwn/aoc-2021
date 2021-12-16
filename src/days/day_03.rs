1ca39f1d9631b712a214b5600e63a82d7f8f4b0e
pngwn <hello@pngwn.io>
2021-12-16 22:22:19 +0000

    day 3 part b

--- src/days/day_03.rs
+++ src/days/day_03.rs
@@ -6,11 +6,12 @@ pub fn run() -> (fn() -> String, fn() -> String) {
 
 fn parta() -> String {
   let moves = process_input(shared::read(3));
-  count_bits(moves).to_string()
+  get_power_consumption(moves).to_string()
 }
 
 fn partb() -> String {
-  "".to_string()
+  let moves = process_input(shared::read(3));
+  get_life_support(moves).to_string()
 }
 
 fn process_input(str: String) -> Vec<Vec<u32>> {
@@ -25,9 +26,15 @@ fn process_input(str: String) -> Vec<Vec<u32>> {
     .collect()
 }
 
-fn count_bits(bits: Vec<Vec<u32>>) -> isize {
-  let mut gamma = "".to_owned();
-  let mut epsilon = "".to_owned();
+#[derive(Debug, PartialEq)]
+enum Frequency {
+  Zeroes,
+  Ones,
+  Equal,
+}
+
+fn count_bit_frequency(bits: Vec<Vec<u32>>) -> Vec<Frequency> {
+  let mut frequency = vec![];
 
   for i in 0..bits[0].len() {
     let (zeros, ones) = bits.iter().fold((0, 0), |acc, next| {
@@ -39,15 +46,96 @@ fn count_bits(bits: Vec<Vec<u32>>) -> isize {
     });
 
     if zeros > ones {
-      gamma.push('0');
-      epsilon.push('1');
+      frequency.push(Frequency::Zeroes)
     } else {
-      gamma.push('1');
-      epsilon.push('0');
+      frequency.push(Frequency::Ones)
+    }
+  }
+
+  frequency
+}
+
+fn get_power_consumption(bits: Vec<Vec<u32>>) -> isize {
+  let mut gamma = "".to_owned();
+  let mut epsilon = "".to_owned();
+
+  for bit in count_bit_frequency(bits) {
+    match bit {
+      Frequency::Zeroes => {
+        gamma.push('0');
+        epsilon.push('1');
+      }
+      Frequency::Ones => {
+        gamma.push('1');
+        epsilon.push('0');
+      }
+      Frequency::Equal => {
+        gamma.push('1');
+        epsilon.push('0');
+      }
     }
   }
 
   isize::from_str_radix(&gamma, 2).unwrap() * isize::from_str_radix(&epsilon, 2).unwrap()
+}
+
+fn get_life_support(bits: Vec<Vec<u32>>) -> isize {
+  let mut co_two = bits.to_vec();
+  let mut o_two = bits.to_vec();
+  let mut i = 0;
+  // println!("{:?} {:?}", bits_copy, bit_freq);
+
+  while co_two.len() > 1 {
+    let bit_freq = count_bit_frequency(co_two.to_vec());
+    let most_frequent: u32 = match bit_freq[i] {
+      Frequency::Zeroes => 0,
+      Frequency::Ones => 1,
+      Frequency::Equal => 9,
+    };
+    // println!("{:?}", bits_copy);
+    co_two = co_two
+      .into_iter()
+      .filter(|x| x[i] == most_frequent)
+      .collect();
+    i += 1;
+  }
+
+  i = 0;
+  while o_two.len() > 1 {
+    let bit_freq = count_bit_frequency(o_two.to_vec());
+    let least_frequent: u32 = match bit_freq[i] {
+      Frequency::Zeroes => 1,
+      Frequency::Ones => 0,
+      Frequency::Equal => 9,
+    };
+    // println!("{:?}", bits_copy);
+    o_two = o_two
+      .into_iter()
+      .filter(|x| {
+        if x[i] == least_frequent {
+          true
+        } else if x[i] == 9 {
+          true
+        } else {
+          false
+        }
+      })
+      .collect();
+    i += 1;
+  }
+
+  // println!("{:?} {:?} {:?}", bits_copy, bit_freq, i);
+
+  isize::from_str_radix(&bits_to_str(co_two[0].to_vec()), 2).unwrap()
+    * isize::from_str_radix(&bits_to_str(o_two[0].to_vec()), 2).unwrap()
+}
+
+fn bits_to_str(bits: Vec<u32>) -> String {
+  bits
+    .iter()
+    .map(|x| x.to_string())
+    .collect::<Vec<String>>()
+    .join("")
 }
 
 #[cfg(test)]
@@ -76,7 +164,7 @@ mod tests {
     )
   }
   #[test]
-  fn count_bits() {
+  fn count_bit_frequency() {
     let input = vec![
       vec![0, 0, 1, 0, 0],
       vec![1, 1, 1, 1, 0],
@@ -92,6 +180,53 @@ mod tests {
       vec![0, 1, 0, 1, 0],
     ];
 
-    assert_eq!(super::count_bits(input.to_owned()), 198)
+    assert_eq!(
+      super::count_bit_frequency(input.to_owned()),
+      vec![
+        super::Frequency::Ones,
+        super::Frequency::Zeroes,
+        super::Frequency::Ones,
+        super::Frequency::Ones,
+        super::Frequency::Zeroes
+      ]
+    )
+  }
+  #[test]
+  fn get_power_consumption() {
+    let input = vec![
+      vec![0, 0, 1, 0, 0],
+      vec![1, 1, 1, 1, 0],
+      vec![1, 0, 1, 1, 0],
+      vec![1, 0, 1, 1, 1],
+      vec![1, 0, 1, 0, 1],
+      vec![0, 1, 1, 1, 1],
+      vec![0, 0, 1, 1, 1],
+      vec![1, 1, 1, 0, 0],
+      vec![1, 0, 0, 0, 0],
+      vec![1, 1, 0, 0, 1],
+      vec![0, 0, 0, 1, 0],
+      vec![0, 1, 0, 1, 0],
+    ];
+
+    assert_eq!(super::get_power_consumption(input.to_owned()), 198)
+  }
+  #[test]
+  fn get_life_support() {
+    let input = vec![
+      vec![0, 0, 1, 0, 0],
+      vec![1, 1, 1, 1, 0],
+      vec![1, 0, 1, 1, 0],
+      vec![1, 0, 1, 1, 1],
+      vec![1, 0, 1, 0, 1],
+      vec![0, 1, 1, 1, 1],
+      vec![0, 0, 1, 1, 1],
+      vec![1, 1, 1, 0, 0],
+      vec![1, 0, 0, 0, 0],
+      vec![1, 1, 0, 0, 1],
+      vec![0, 0, 0, 1, 0],
+      vec![0, 1, 0, 1, 0],
+    ];
+
+    assert_eq!(super::get_life_support(input.to_owned()), 230)
   }
 }
